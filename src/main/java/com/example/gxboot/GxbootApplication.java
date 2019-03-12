31492c21e532220cadb2ce1bae03201b020c91c4
guxing <guxing@cloudwalk.cn>
2019-03-12 16:42:14 +0800

    初始化工程

---
+++ src/main/java/com/example/gxboot/GxbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.gxboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GxbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GxbootApplication.class, args);
+	}
+
+}
