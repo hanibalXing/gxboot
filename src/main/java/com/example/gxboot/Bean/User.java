b3183da0ff34142105079f48768e1b4c83bf1cad
guxing <guxing@cloudwalk.cn>
2019-03-13 17:15:29 +0800

    初始化工程

---
+++ src/main/java/com/example/gxboot/Bean/User.java
@@ -0,0 +1,20 @@
+package com.example.gxboot.Bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * ClassName: User<br/>
+ * Description: java类作用描述<br/>
+ * date 2019/3/13 15:59
+ *
+ * @author gx
+ * @Version 1.0
+ * @since 1.7
+ */
+@Data
+@AllArgsConstructor
+public class User {
+	private String userName;
+	private Integer age;
+}
---
+++ src/main/java/com/example/gxboot/controller/TestController.java
@@ -0,0 +1,52 @@
+package com.example.gxboot.controller;
+
+import com.example.gxboot.Bean.User;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+/**
+ * ClassName: TestController<br/>
+ * Description: java类作用描述<br/>
+ * date 2019/3/13 14:44
+ *
+ * @author gx
+ * @Version 1.0
+ * @since 1.7
+ */
+@RestController
+@Slf4j
+public class TestController {
+	@RequestMapping(path = "/hi", method = POST)
+	public String add(@RequestBody User user) {
+		return "123";
+	}
+
+	@RequestMapping(path = "/hi/{id}", method = GET)
+	public String get(@PathVariable Long id) {
+		return "234";
+	}
+
+	@RequestMapping(path = "/hi/{id}", method = GET)
+	public String page(@PathVariable Long id) {
+		return "235";
+	}
+
+	@RequestMapping(path = "/hi/{id}", method = PUT)
+	public String update(@PathVariable Long id, @RequestBody User user) {
+		log.info("user:{}", user);
+		return "345";
+	}
+
+	@RequestMapping(path = "/hi", method = DELETE)
+	public String delete() {
+		return "456";
+	}
+
+
+}
